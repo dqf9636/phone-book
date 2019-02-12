e62195b305fb6cf74ab5357a1fc1e6bc6bfc120c
BORAM <dqf9636@gmail.com>
2019-02-12 16:38:29 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Let's start!
           </a>
         </header>
       </div>
